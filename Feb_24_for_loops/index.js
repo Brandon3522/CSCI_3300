349ddd0383ad40f4f002f6f9dc85b0940bac8f76
Brandon Sitz <Brandonsitz17@gmail.com>
2022-02-25 21:26:27 -0600

    Assignment 3

--- Feb_24_for_loops/index.js
+++ Feb_24_for_loops/index.js
@@ -21,13 +21,51 @@ for (let index = 0; index < 5; index++) {
 }
 
 console.log('===============================================')
-// For loop shapes
-function execute() {
-    
+// Test
+console.log(typeof(50));
+
+let num = 10.55;
+if (num % 1 > .50) {
+    console.log(parseInt(num) + 1)
 }
+else {
+    console.log(parseInt(num))
+}
+console.log(10.15 / 100 * 100);
+console.log(10.15 % 1);
 
-console.log('Enter a positve number: ');
+console.log("========================================")
 
+let firstNum = 8;
+let secondNum = 25.25;
+let thirdNum = 15;
 let result = '';
 
+if (firstNum == secondNum && secondNum == thirdNum) {
+    result = `The only number (${firstNum}) is the middle number.`;
+} else if (firstNum == secondNum) {
+    result = `A number is repeated (${firstNum}) is the repeated number.`;
+}
+else if (secondNum == thirdNum) {
+    result = `A number is repeated (${secondNum}) is the repeated number.`;
+}
+else if (
+    (firstNum < secondNum && firstNum > thirdNum) ||
+    (firstNum > secondNum && firstNum < thirdNum)
+) {
+    result = `The first number (${firstNum}) is the middle number.`;
+} else if (
+    (secondNum < firstNum && secondNum > thirdNum) ||
+    (secondNum > firstNum && secondNum < thirdNum)
+) {
+    result = `The second number (${secondNum}) is the middle number.`;
+} else if (
+    (thirdNum < firstNum && thirdNum > secondNum) ||
+    (thirdNum > firstNum && thirdNum < secondNum)
+) {
+    result = `The third number (${thirdNum}) is the middle number.`;
+}
 
+console.log(result);
+
+console.log(typeof 'secondNum')
